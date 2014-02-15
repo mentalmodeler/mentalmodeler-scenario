728d09d78c78c86f21bdd32ce489994f23451c37
jonathan elbom <jb@jonnybomb.com>
2014-02-15 13:24:15 -0600

    tweak files ui styles and made r.js dist build

---
+++ scripts/build.js
@@ -0,0 +1,11 @@
+({
+    baseUrl: './',
+    //optimize: 'none', //uncomment this option if built source needs to be debugged
+    paths: {
+    	almond: '../vendor/almond/almond'
+    },
+    mainConfigFile: 'require-config.js',
+    name: 'almond',
+    include: ['main'],
+    out: '../dist/scripts/mentalmodeler.js'
+})

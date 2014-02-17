cb8c84e94df236ae864a66e8419eb7ddd7c118ce
arousset <andrewrousset@gmail.com>
2014-02-17 17:58:19 -0600

    fix for firefox swf modeler

--- scripts/views/modeling.js
+++ scripts/views/modeling.js
@@ -11,13 +11,14 @@ define([
     'use strict';
 
     var ModelingView = AbstractView.extend({
-        
+
         tagName: 'div',
         id: 'flash-model',
-        
+        flash: null,
+
         events: {
         },
-        
+
         initialize: function() {
             ModelingView.__super__.initialize.apply( this, arguments );
             //Backbone.on( 'window:resize', this.onWindowResize, this );
@@ -27,11 +28,14 @@ define([
             this.listenTo( Backbone, 'section:change', this.onSectionChange );
         },
 
-        render: function() {            
-            this.$el.flash({ swf: 'swf/mentalmodeler.swf',
+        render: function() {
+            /*this.$el.flash({ swf: 'swf/mentalmodeler.swf',
                              height: '100%',
-                             width: '100%'
-            });
+                             width: '100%',
+                             id: 'flash-content'
+            });*/
+            this.$el.append("<embed id='flash-content' src='swf/mentalmodeler.swf' height='100%' width='100%' allowscriptaccess='always' type='application/x-shockwave-flash' />");
+            this.flash = this.$el.find('#flash-content')[0];
             return this;
         },
 
@@ -42,6 +46,7 @@ define([
                    xml = this.doSave();
                 }
             );
+            xml = this.flash.doSave();
             return xml;
         },
 
@@ -49,31 +54,32 @@ define([
              this.$el.flash(
                 function() {
                     if ( this.hasOwnProperty('doLoad') ) {
-                        this.doLoad( xml );    
+                        this.doLoad( xml );
                     }
                     else {
                         console.log('--ERROR-- ModelingView, reload swf > no doLoad in this:',this)
                     }
-                    
+
                 }
-            );   
+            );
+            this.flash.doLoad( xml );
         },
 
         onSelectionChange: function( model, target, section ) {
             //console.log('ModelingView > onSelectionChange, model:',model,', target:',target,', section:',section);
             var appModel = window.mentalmodeler.appModel;
             if ( appModel.curSection === 'modeling' && appModel.curModel ) {
-                this.reloadSwf( appModel.curModel.getModelingXML() ); 
-            }  
+                this.reloadSwf( appModel.curModel.getModelingXML() );
+            }
         },
 
         onSectionChange: function( section ) {
             //console.log('ModelingView > onSelectionChange');
             var appModel = window.mentalmodeler.appModel;
             if ( section === 'modeling' && appModel.curModel ) {
-                this.reloadSwf( appModel.curModel.getModelingXML() ); 
+                this.reloadSwf( appModel.curModel.getModelingXML() );
             }
-            
+
         }
     });
 
